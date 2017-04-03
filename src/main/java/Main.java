a7635ef5f0b7af3a355eec443982ff1b4e1310e1
Vladimir Druzhinin <vdruzhinin@rhythmone.com>
2017-04-03 09:22:58 +0700

    add first project java

---
+++ src/main/java/Main.java
@@ -0,0 +1,18 @@
+public class Main{
+
+    public static void main(String[] args){
+        int[] arr = {1,2,5,7,9};
+        int max = findArr(arr);
+        System.out.println(max);
+    }
+
+    public static int findArr(int[] brr ){
+        int max = 0;
+        for (int i = 0;i < brr.length; i++) {
+            if(max < brr[i]){
+                max = brr[i];
+            }
+        }
+        return max;
+    }
+}
