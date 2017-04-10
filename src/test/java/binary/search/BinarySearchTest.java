fe6e641c9ab559a1767279063f8bdd9788ef70e1
Vladimir Druzhinin <vdruzhinin@rhythmone.com>
2017-04-10 16:58:34 +0700

    add tast cases

--- src/test/java/binary/search/BinarySearchTest.java
+++ src/test/java/binary/search/BinarySearchTest.java
@@ -14,5 +14,33 @@ public class BinarySearchTest {
     @Test
     public void test() {
         assertEquals(0, binarySearch(new int[]{1,2,3,4,5}, 0, 5, 1));
+        assertEquals(1, binarySearch(new int[]{1,2,3,4,5}, 0, 5, 2));
+        assertEquals(2, binarySearch(new int[]{1,2,3,4,5}, 0, 5, 3));
+        assertEquals(3, binarySearch(new int[]{1,2,3,4,5}, 0, 5, 4));
+        assertEquals(4, binarySearch(new int[]{1,2,3,4,5}, 0, 5, 5));
+        assertEquals(-1, binarySearch(new int[]{1,2,3,4,5,7}, 0, 6, 6));
+        assertEquals(-1, binarySearch(new int[]{1,3,4,5,6,7}, 0, 6, 2));
+        assertEquals(-1, binarySearch(new int[]{1,2,3,5,6,7}, 0, 6, -1));
+        assertEquals(-1, binarySearch(new int[]{1,2,3,5,6,7}, 0, 6, 10));
+
+
+
+
+        assertEquals(0, bSearch(new int[]{1}, 1));
+        assertEquals(1, bSearch(new int[]{1, 1}, 1));
+        assertEquals(0, bSearch(new int[]{1, 3}, 1));
+        assertEquals(1, bSearch(new int[]{1, 3}, 3));
+        assertEquals(-1, bSearch(new int[]{1}, 0));
+        assertEquals(-1, bSearch(new int[]{1}, 2));
+        assertEquals(-1, bSearch(new int[]{1, 1}, 0));
+        assertEquals(-1, bSearch(new int[]{1, 1}, 2));
+        assertEquals(-1, bSearch(new int[]{1, 2}, 0));
+        assertEquals(-1, bSearch(new int[]{1, 2}, 3));
+        assertEquals(-1, bSearch(new int[]{}, 3));
+        assertEquals(1, bSearch(new int[]{1, 2, 3}, 2));
+        assertEquals(1, bSearch(new int[]{1, 2, 3, 4}, 2));
+        assertEquals(2, bSearch(new int[]{1, 2, 3, 4}, 3));
+
+
     }
 }
