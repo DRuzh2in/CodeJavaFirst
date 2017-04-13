e49bd84b1c9c9388d46853a97b54b9d46ba28a22
Vladimir Druzhinin <vdruzhinin@rhythmone.com>
2017-04-13 15:35:11 +0700

    merge array

---
+++ src/main/java/array/Array.java
@@ -0,0 +1,29 @@
+package array;
+
+/**
+ * Created by vdruzhinin on 4/13/2017.
+ */
+public class Array {
+
+    public static int[] mergArray(int[] arrayA, int[] arrayB) {
+        int[] arrayC = new int[100];
+        boolean flag = false;
+
+        for (int i = 0, k = 0; i < arrayA.length; i++) {
+            for (int j = 0; j < arrayB.length; j++) {
+                if (arrayB[j] == arrayA[i]) {
+                    flag = true;
+                    for (int n = 0; n < k; n++) {
+                        if (arrayC[n] == arrayA[i]) {
+                            flag = false;
+                        }
+                    }
+                }
+            }
+            if (flag) {
+                arrayC[k++] = arrayA[i];
+            }
+        }
+        return arrayC;
+    }
+}
